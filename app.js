98bc099a35010929bd5a83acfd848c66c79ccca3
Abdou Lahat DEME <lahad.97@gmail.com>
2019-04-09 12:44:24 +0200

    add config and code for the ultrasound sensor

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const fs = require('fs')
 const arduino_config = require('./config/arduino')
-const stm_config = require('./config/stm32')
+const stm_ir_config = require('./config/stm32_IR')
+const stm_ultrasound_config = require('./config/stm32_ultrasound')
 const SerialPort = require('serialport')
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
 
-const stm_port = new SerialPort(stm_config.port, { baudRate: stm_config.baudRate })
+const stm_ir_port = new SerialPort(stm_ir_config.port, { baudRate: stm_ir_config.baudRate })
+const stm_ultrasound_port = new SerialPort(stm_ultrasound_config.port, { baudRate: stm_ultrasound_config.baudRate })
 const arduino_port = new SerialPort(arduino_config.port, { baudRate: arduino_config.baudRate })
 
-stm_port.on('data', (data) => {
-    fs.appendFile(`${stm_config.data_rep}/test.csv`, data.toString(), (err, data) => {
+stm_ir_port.on('data', (data) => {
+    fs.appendFile(`${stm_ir_config.data_rep}/test.csv`, data.toString(), (err) => {
         if (err) console.log(err)
-        console.log(data)
     })
 })
 
 arduino_port.on('data', (data) => {
-    fs.appendFile(`${arduino_config.data_rep}/test.csv`, data.toString(), (err, data) => {
+    fs.appendFile(`${arduino_config.data_rep}/test.csv`, data.toString(), (err) => {
         if (err) console.log(err)
-        console.log(data)
     })
 })
